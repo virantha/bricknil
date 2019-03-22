a403246c4b935b8079295fc2f0b21ed6a347eead
Virantha Ekanayake <virantha@gmail.com>
2019-03-22 12:31:57 -0400

    made duplo example cleaner

--- examples/duplo_train.py
+++ examples/duplo_train.py
@@ -12,35 +12,32 @@ import logging
 @attach(LED, name='led')
 @attach(DuploSpeedSensor, name='speed_sensor', capabilities=['sense_speed', 'sense_count'])
 @attach(DuploTrainMotor, name='motor')
-class Robot(DuploTrainHub):
+class Train(DuploTrainHub):
 
     async def speed_sensor_change(self):
         speed = self.speed_sensor.value[DuploSpeedSensor.capability.sense_speed]
         count = self.speed_sensor.value[DuploSpeedSensor.capability.sense_count]
-        logging.info(f'Speed sensor changed speed: {speed} count: {count}')
+        self.message_info(f'Speed sensor changed speed: {speed} count: {count}')
 
     async def vision_sensor_change(self):
         cap = DuploVisionSensor.capability
         color = self.vision_sensor.value[cap.sense_color]
         ctag  = self.vision_sensor.value[cap.sense_ctag]
         reflt  = self.vision_sensor.value[cap.sense_reflectivity]
-        logging.info(f'Vision sensor changed color: {color} ctag: {ctag} reflt: {reflt}')
-
-    async def motor_change(self):
-        pass
-    async def tilt_sensor_change(self):
-        pass
+        self.message_info(f'Vision sensor changed color: {color} ctag: {ctag} reflt: {reflt}')
 
     async def run(self):
         self.message_info("Running")
 
         colors = cycle([Color.red, Color.purple, Color.yellow, Color.blue, Color.white])
+
         snd = DuploSpeaker.sounds
         sounds = cycle([snd.brake, snd.station, snd.water, snd.horn, snd.steam])
+
         for i in range(5):
-            await self.led.set_color(next(colors))
-            await self.speaker.play_sound(next(sounds))
-            tgt_speed = 30 + i*15
+            await self.led.set_color(next(colors))       # Cycle through the colors
+            await self.speaker.play_sound(next(sounds))  # cycle through the sounds
+            tgt_speed = 20 + i*15                        # Keep increasing the speed
             await self.motor.ramp_speed(tgt_speed, 2000)
             self.message_info(f"Set speed to {i}")
             await sleep(3)
@@ -48,7 +45,7 @@ class Robot(DuploTrainHub):
         self.message_info("Done")
 
 async def system():
-    hub = Robot('robot')
+    hub = Train('train')
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
